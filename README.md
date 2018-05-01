0ad78df792ce1940cd0052f1e042a5abd81c9f5c
Junjie Huang <acmhjj@gmail.com>
2018-05-02 02:27:03 +0800

    complete the course, upload all my solutions

--- README.md
+++ README.md
@@ -6,7 +6,74 @@ This is all written in matlab.
 
 ## Includes:
 
-* Linear Regression (one variable and multiple variables)
-* Logistic Regression
-* Regularization
-* Neural networks
+* Week 1
+	- Introduction
+	    + Welcome
+	    + Introduction
+	- Linear Regression with One Variable
+	    + Model and Cost Function
+	    + Parameter Learning
+	- Linear Algebra Review
+		+ Linear Algebra Review
+* Week 2
+	- Linear Regression with Multiple Variables
+	    + Environment Setup Instructions
+	    + Multivariate Linear Regression
+	    + Computing Parameters Analytically
+	    + Submitting Programming Assignments
+  - Octave/Matlab Tutorial
+	    + Octave/Matlab Tutorial
+* Week 3
+  - Logistic Regression
+	    + Classification and Representation
+	    + Logistic Regression Model
+	    + Multiclass Classification
+  - Regularization
+	    + Solving the Problem of Overfitting
+* Week 4
+  - Neural Networks: Representation
+	    + Motivations
+	    + Neural Networks
+	    + Applications
+* Week 5
+  - Neural Networks: Learning
+	    + Cost Function and Backpropagation
+	    + Backpropagation in Practice
+	    + Application of Neural Networks
+* Week 6
+  - Advice for Applying Machine Learning
+	    + Evaluating a Learning Algorithm
+	    + Bias vs. Variance
+  - Machine Learning System Design
+	    + Building a Spam Classifier 
+	    + Handling Skewed Data
+	    + Using Large Data Sets
+* Week 7
+  - Support Vector Machines
+	    + Large Margin Classification
+	    + Kernels
+	    + SVMs in Practice
+* Week 8
+  - Unsupervised Learning
+	    + Clustering
+  - Dimensionality Reduction
+	    + Motivation
+	    + Principal Component Analysis
+	    + Applying PCA
+* Week 9
+  - Anomaly Detection
+	    + Density Estimation
+	    + Building an Anomaly Detection System
+	    + Multivariate Gaussian Distribution (Optional)
+  - Recommender Systems
+	    + Predicting Movie Ratings
+	    + Collaborative Filtering
+	    + Low Rank Matrix Factorization
+* Week 10
+  - Large Scale Machine Learning
+	    + Gradient Descent with Large Datasets
+	    + Advanced Topics
+* Week 11
+  - Application Example: Photo OCR
+	    + Photo OCR
+	    + Conclusion
--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices.
-%
+%   nn_params and need to be converted back into the weight matrices. 
+% 
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-
-% You need to return the following variables correctly
+         
+% You need to return the following variables correctly 
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a
+%               containing values from 1..K. You need to map this vector into a 
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the
+%               over the training examples if you are implementing it for the 
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -61,18 +61,50 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
+X = [ones(m, 1) X];
+z1 = X * Theta1';
+a1 = sigmoid(z1);
+a1 = [ones(m, 1) a1];
+z2 = a1 * Theta2';
+h = sigmoid(z2);
+%Z2 = Theta1*X';
+%Z2 = [ones(1,m);Z2];
+%Z3 = Theta2*sigmoid(Z2);
+%h = sigmoid(Z3);
+for k=1:num_labels
+yk = y==k;
+J = J - (1/m)*sum(((yk.*log(h(:,k))) + (((1-yk).* log(1-h(:,k))))));
+end
+rtheta1 = sum(sum(Theta1(:,2:end).^2));
+rtheta2 = sum(sum(Theta2(:,2:end).^2));
+bias = lambda/(2*m);
 
-y = eye(num_labels)(:, y);
+J = J + (bias * (rtheta1+rtheta2));
+Del1=0;
+Del2=0;
+for t=1:m
+   A1 = X(t,:)';
+   Z2 = Theta1*A1;
+   A2 = [1;sigmoid(Z2)];
+   Z3 = Theta2*A2;
+   H = sigmoid(Z3);
+%    for k=1:num_labels
+%       yk = y==k;
+%       del3(k) = h(t,k) - yk;
+%    end
+    actual = y(t,:);
+    yk = zeros(num_labels,1);
+    yk(actual) = 1;
+    del3 = H-yk;
+   del2 = (Theta2(:,2:end)'*del3) .* sigmoidGradient(Z2);
+%    del1 = Theta1'*del3 .* sigmoidGradient(Z1);
+   Del1 = Del1 + del2* A1';
+   Del2 = Del2 + del3* A2';
+end
+Theta1_grad = Del1/m + (lambda/m)*[zeros(hidden_layer_size,1) Theta1(:,2:end)];
+Theta2_grad = Del2/m + (lambda/m)*[zeros(num_labels,1) Theta2(:,2:end)];
 
-a1 = [ones(m, 1) X];
-z2 = a1 * Theta1';
-a2 = sigmoid(z2);
 
-a2 = [ones(m, 1) a2];
-z3 = a2 * Theta2';
-a3 = sigmoid(z3)
-
-%logistic_cost = (-y) .* log(a3) - (1 - y) .* log(1 - a3);
 
 
 % -------------------------------------------------------------
